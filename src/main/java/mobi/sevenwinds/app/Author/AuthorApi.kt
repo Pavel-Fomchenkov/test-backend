cf206a660e93b9e8de541664eaee591162f7ea00
Pavel-Fomchenkov <donotwritemail@gmail.com>
2025-01-22 23:02:23 +0300

    simple test method added

---
+++ src/main/java/mobi/sevenwinds/app/Author/AuthorApi.kt
@@ -0,0 +1,31 @@
+package mobi.sevenwinds.app.Author
+
+import com.papsign.ktor.openapigen.annotations.parameters.QueryParam
+import com.papsign.ktor.openapigen.route.info
+import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
+import com.papsign.ktor.openapigen.route.path.normal.get
+import com.papsign.ktor.openapigen.route.path.normal.post
+import com.papsign.ktor.openapigen.route.response.respond
+import com.papsign.ktor.openapigen.route.route
+import mobi.sevenwinds.app.budget.BudgetService
+import java.time.LocalDateTime
+
+fun NormalOpenAPIRoute.author() {
+    route("/author") {
+        route("/test").get<Unit, AuthorResponse>(info("Получить тестовый ответ")) {
+            respond(AuthorResponse("Привет"))
+        }
+        route("/add").post<Unit, AuthorRecord, AuthorRecord>(info("Добавить автора")) {
+                param, body -> respond(body)
+        }
+    }
+}
+
+data class AuthorRecord(
+    val name: String,
+    val entryDate: LocalDateTime
+)
+
+class AuthorResponse(
+    val hello: String
+)
